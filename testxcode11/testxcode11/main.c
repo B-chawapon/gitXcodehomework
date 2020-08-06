bb1b8a18d0c70ba7460ac3f881d5aca081cea2a1
Chawapon <chawapon2001@gmail.com>
2020-08-06 19:40:51 +0700

    pass

---
+++ testxcode11/testxcode11/main.c
@@ -0,0 +1,16 @@
+//
+//  main.c
+//  testxcode11
+//
+//  Created by Chawapon Baidumrongsak on 8/6/20.
+//  Copyright © 2020 Chawapon Baidumrongsak. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    printf("test\n");
+    return 0;
+}
