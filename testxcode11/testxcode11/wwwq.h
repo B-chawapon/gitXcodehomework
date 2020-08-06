69aac3f10c555c5219558d5b8b7d218ebf721625
Chawapon <chawapon2001@gmail.com>
2020-08-06 19:46:08 +0700

    pass this  www

---
+++ testxcode11/testxcode11/wwwq.h
@@ -0,0 +1,14 @@
+//
+//  wwwq.h
+//  testxcode11
+//
+//  Created by Chawapon Baidumrongsak on 8/6/20.
+//  Copyright © 2020 Chawapon Baidumrongsak. All rights reserved.
+//
+
+#ifndef wwwq_h
+#define wwwq_h
+
+#include <stdio.h>
+
+#endif /* wwwq_h */
