da866b5d5fbf98f083022c6f640abbf012b8ffce
Chawapon <68158123+nutt07@users.noreply.github.com>
2020-09-04 21:56:00 +0700

    add function det

--- detMatrix3x3/detMatrix3x3/matrix3x3.cpp
+++ detMatrix3x3/detMatrix3x3/matrix3x3.cpp
@@ -1,9 +1,10 @@
 #include <stdio.h>
-int main()
-{
     int i,j;
     int det=0;
     int num[3][5];
+    int determinant(int a);
+int main()
+{
     for(i=0;i<3;i++)
     {
         for(j=0;j<3;j++)
@@ -21,8 +22,13 @@ int main()
        }
     for(i=0;i<3;i++)
     {
-        det = det + (num[0][i]*(num[1][(i+1)%3]*num[2][(i+2)%3] - num[1][(i+2)%3]*num[2][(i+1)%3]));
+        determinant(det);
     }
     printf("\n%d\n",det);
     return 0;
+}
+int determinant(int a)
+{
+    det = det + (num[0][i]*(num[1][(i+1)%3]*num[2][(i+2)%3] - num[1][(i+2)%3]*num[2][(i+1)%3]));
+    return det;
 }
