af7e011db2d1c8e9d756608a35cf02139a124b05
Chawapon <chawapon2001@gmail.com>
2020-10-12 17:08:51 +0700

    update days
    fix bug and
    update days

--- strucdeltatime/strucdeltatime/main.cpp
+++ strucdeltatime/strucdeltatime/main.cpp
@@ -6,7 +6,6 @@ struct time
     int hours;
     int mins;
     int seconds;
-
 };
 void finddeltatime(struct time timestat[2],struct time *diff);
 int i;
@@ -20,28 +19,25 @@ int main()
         {
             printf("Enter speed1 hours mins seconds : ");
             scanf("%d %d %d",&timestat[i].hours,&timestat[i].mins,&timestat[i].seconds);
-            printf("Time stat 1 is %d:%d:%d\n",timestat[i].hours,timestat[i].mins,timestat[i].seconds);
+            printf("Time speed1 is %d:%d:%d\n",timestat[i].hours,timestat[i].mins,timestat[i].seconds);
         }
         else{
             printf("Enter speed2 hours mins seconds : ");
             scanf("%d %d %d",&timestat[i].hours,&timestat[i].mins,&timestat[i].seconds);
-            printf("Time stat 2 is %d:%d:%d\n",timestat[i].hours,timestat[i].mins,timestat[i].seconds);
+            printf("Time speed2 is %d:%d:%d\n",timestat[i].hours,timestat[i].mins,timestat[i].seconds);
         }
         
     }
     finddeltatime(timestat,&diff);
-    printf("%d",abs(diff.hours));
-    printf(":%d",abs(diff.mins));
-    printf(":%d",abs(diff.seconds));
-   
+    printf("%d hrs %d mins %d secs\n",(diff.hours),(diff.mins),(diff.seconds));
     
-    return 0;
+   return 0;
     
 }
 void finddeltatime(struct time timestat[2],struct time *diff)
 {
     struct time totalseconds[2];
-    int diftempsec,mintemp;
+    int diftempsec,mintemp,date;
     for(i=0;i<1;i++)
     {
        totalseconds[i].seconds =((((timestat[i].hours*60)+timestat[i].mins)*60)+timestat[i].seconds);
@@ -51,17 +47,32 @@ void finddeltatime(struct time timestat[2],struct time *diff)
         
         diftempsec=totalseconds[i].seconds-totalseconds[i+1].seconds;
         //printf("\n%d ",diftempsec);
+        if(diftempsec<0)
+        {
+            diftempsec=diftempsec*-1;
+            mintemp=diftempsec/60;
+            diff->seconds = (diftempsec%60);
+            diff->mins = (mintemp%60);
+            if((mintemp/60)>23)
+            {
+                date=(mintemp/60)/24;
+                printf("%d Day(s) with ",date);
+                diff->hours=(mintemp/60)%24;
+            }
+        }
+        else
+        {
+            mintemp=diftempsec/60;
+            diff->seconds = (diftempsec%60);
+            diff->mins = (mintemp%60);
+            if((mintemp/60)>23)
+            {
+                date=(mintemp/60)/24;
+                printf("%d Day(s) with ",date);
+                diff->hours=(mintemp/60)%24;
+            }
+        }
         
-        mintemp=diftempsec/60;
-
-        diff->seconds = (diftempsec%60);
-        diff->mins = (mintemp%60);
-        diff->hours = (mintemp/60);
+        
     }
-    
-   
-  
-   
-   
-
 }
